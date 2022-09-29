4e00e761fb82d47be9aee7f37660f6bf91b86999
David Caro <david.caro.dec@hotmail.com>
2022-09-29 17:55:28 -0300

    Exersise 3 finished

--- Exersise 3/src/App.java
+++ Exersise 3/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] numbers = new int[100];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random()*200 + 1);
+        }
+        Arrays.sort(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
+
 }
